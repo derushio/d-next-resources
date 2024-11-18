dc685eca954c4c351e30f8a22625f5c8a596983e
derushio <derushio@gmail.com>
2024-11-18 14:02:06 +0900

    signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+'use server';
+
+import { SigninForm } from '@/components/auth/signin/SigninForm';
+
+export default async function SigninPage() {
+  return (
+    <div className='flex w-full h-full justify-center items-center'>
+      <SigninForm />
+    </div>
+  );
+}
