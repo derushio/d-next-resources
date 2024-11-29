121842e51ed61fa92049cf9be454c50271b548b7
derushio <derushio@gmail.com>
2024-11-29 19:22:43 +0900

    filterNull

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,5 @@
 export function contains<T>(arr1: T[], arr2: T[]): boolean {
   return arr2.some((item) => arr1.includes(item));
 }
+
+export const filterNull = <T>(v: T): v is NonNullable<T> => v != null;
