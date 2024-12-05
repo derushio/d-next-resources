ee574963fa73749c22726dba04c026b109c25476
derushio <derushio@gmail.com>
2024-12-05 19:33:14 +0900

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,51 @@
 import * as flowbite from 'flowbite-react/tailwind';
 import type { Config } from 'tailwindcss';
 
+const gap = {
+  '0.25em': '0.25em',
+  '0.5em': '0.5em',
+  '1em': '1em',
+  '2em': '2em',
+  '3em': '3em',
+  '4em': '4em',
+};
+
+const widthHeight = {
+  '128': '32rem',
+  '256': '64rem',
+  '1px': '1px',
+  '2px': '2px',
+  '0.1em': '0.1em',
+  '0.2em': '0.2em',
+  '4em': '4em',
+  '8em': '8em',
+  '14em': '14em',
+  '16em': '16em',
+  '18em': '18em',
+  '40em': '40em',
+  '10%': '10%',
+  '20%': '20%',
+  '30%': '30%',
+  '40%': '40%',
+  '50%': '50%',
+  '60%': '60%',
+  '70%': '70%',
+  '80%': '80%',
+  '90%': '90%',
+};
+
+const inset = {
+  '10%': '10%',
+  '20%': '20%',
+  '30%': '30%',
+  '40%': '40%',
+  '50%': '50%',
+  '60%': '60%',
+  '70%': '70%',
+  '80%': '80%',
+  '90%': '90%',
+};
+
 export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -19,22 +64,43 @@ export default {
         error2: 'var(--error2)',
         warn: 'var(--warn)',
         warn2: 'var(--warn2)',
+        paper: 'var(--paper)',
       },
       transitionProperty: {
         width: 'width',
         margin: 'margin',
       },
-      width: {
-        '128': '32rem',
-        '256': '64rem',
-      },
+      width: widthHeight,
+      height: widthHeight,
       maxWidth: {
         '128': '32rem',
         '192': '45rem',
         '256': '64rem',
         '512': '128rem',
       },
-      inset: {},
+      inset: inset,
+      aspectRatio: {
+        a4: '1 / 1.41421356',
+      },
+      fontSize: {
+        '50%': '0.500em',
+        '75%': '0.750em',
+        '80%': '0.800em',
+        '85%': '0.850em',
+        '100%': '1.000em',
+        '110%': '1.100em',
+        '120%': '1.200em',
+        '130%': '1.300em',
+        '150%': '1.500em',
+        '200%': '2.000em',
+        '250%': '2.500em',
+        '300%': '3.000em',
+        '400%': '4.000em',
+        '500%': '5.000em',
+      },
+      gap: gap,
+      padding: gap,
+      margin: gap,
     },
   },
   plugins: [flowbite.plugin()],
