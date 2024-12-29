dc0488cd0715ef3938a7549111472aef9eaf7369
derushio <derushio@gmail.com>
2024-12-30 01:57:49 +0900

    feat(settings): add src/utils to Default group and implement genHash function

---
+++ src/utils/hash.ts
@@ -0,0 +1,11 @@
+import { Env } from '@/data-accesses/queries/env/Env';
+import { upperzero } from '@/data-accesses/types/zod/utils';
+import bcrypt from 'bcrypt';
+import { z } from 'zod';
+
+export async function genHash(text: string) {
+  return await bcrypt.hash(
+    text,
+    upperzero(z.number()).parse(Number(Env.TOKEN_SALT_ROUNDS)),
+  );
+}
